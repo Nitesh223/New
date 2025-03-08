6ae2ad91c15a9ee3d1f13561b20b6e92cfaf86f9
Nitesh223 <nitesh725025@gmail.com>
2025-03-09 01:57:33 +0530

    new update

--- practice1.js
+++ practice1.js
@@ -1,11 +1,47 @@
- 
-function add(){
-    var firstvalue = document.getElementById("firstinput").value;
-    var secondvalue = document.getElementById("secondinput").value;
-    
-    var  add = parseInt(firstvalue) + parseInt(secondvalue)
+// function calculator(Nitesh){
+//     var firstvalue = parseFloat(document.getElementById("firstvalue").value);
+//     var secondvalue = parseFloat(document.getElementById("secondvalue").value);
 
-    //alert (add)
-    document.getElementById("result").innerHTML="result is"+ "    "+ add
-    // document.getElementById("result").innerHTML = `result is `
+//     switch(Nitesh)
+//     {
+//         case "add" :
+//             document.getElementById("result").innerHTML = "The sum of first value and second value is " + (parseInt(firstvalue) + parseInt(secondvalue));
+//             break;
+//             case "subtract" :
+//             document.getElementById("result").innerHTML = firstvalue-secondvalue;
+//             break;
+//             case "multiply" :
+//             document.getElementById("result").innerHTML = firstvalue*secondvalue;
+//             break;
+//             case "divide" :
+//             document.getElementById("result").innerHTML = firstvalue/secondvalue;
+//             break;
+//     }
+// }
+function student() {
+    var firstname = document.getElementById("firstname").value;
+    var secondname = document.getElementById("secondname").value;
+    var mobile = document.getElementById("mobile").value;
+    var dob = document.getElementById("dob").value;
+    var male = document.getElementById("male")
+    var Female = document.getElementById("Female")
+    var maths = document.getElementById("maths").value
+    var science = document.getElementById("science").value;
+    var sst = document.getElementById("sst").value;
+    var english = document.getElementById("english").value;
+    var sanskrit = document.getElementById("sanskrit").value;
+    var total = parseInt(english) + parseInt(sanskrit) + parseInt(maths) + parseInt(science) + parseInt(sst)
+    var percentage = (parseInt(english) + parseInt(sanskrit) + parseInt(maths) + parseInt(science) + parseInt(sst)) / 5
+    var file = document.getElementById("file").files[0]?.name
+    console.log(file)
+    var gender = ""
+    if (male.checked){
+        gender="male"
+    }
+    else if(Female.checked){
+        gender="Female"
+    }
+    document.getElementById("result1").innerHTML = `Your name is ${firstname} ${secondname} <div> ${mobile} </div> Your date of birth is : ${dob} , Gender : ${gender}<div>Marks in Maths : ${maths} , Marks in Science : ${science} ,Marks in  SST : ${sst} ,Marks in English : ${english} ,Marks in Sanskrit : ${sanskrit} , </div> <div> Your Total Marks is : ${total} ,Percentage : ${percentage}</div> <div><img src = "${file}">${file}</div>`
+
+    console.log("Nitesh")
 }
